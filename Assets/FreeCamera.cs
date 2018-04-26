4c3051a5a870e6738e25aff309eccc55bfcdf29b
MRover <rover@umich.edu>
2018-04-26 12:24:08 -0400

    First Commit

---
+++ Assets/FreeCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeCamera : MonoBehaviour {
+
+	// Use this for initialization
+    private float phi=120, theta=40;
+    public float phiSpeed, thetaSpeed;
+	
+	// Update is called once per frame
+    void Update () {
+        if(Input.GetKey(KeyCode.LeftArrow))
+            phi+=phiSpeed*Time.deltaTime;
+        if(Input.GetKey(KeyCode.RightArrow))
+            phi-=phiSpeed*Time.deltaTime;
+        if(Input.GetKey(KeyCode.UpArrow))
+            theta+=thetaSpeed*Time.deltaTime;
+        if(Input.GetKey(KeyCode.DownArrow))
+            theta-=thetaSpeed*Time.deltaTime;
+
+        transform.position=Quaternion.AngleAxis(phi, Vector3.up)*Vector3.forward*15;
+        transform.LookAt(Vector3.zero);
+        transform.position = Quaternion.AngleAxis(theta, transform.right)*transform.position;
+        transform.LookAt(Vector3.zero);
+        transform.position+=Vector3.back*3;
+	}
+}
---
+++ Assets/Joint.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngineInternal.Input;
+using System;
+
+public class Joint : MonoBehaviour {
+
+    public Vector3 localRotationAxis;
+    public float minAngle, maxAngle;
+    public float rotationSpeed;
+    public KeyCode positive, negative;
+    public string posJoy, negJoy, posButton, negButton;
+
+    private float angle;
+
+	// Use this for initialization
+	void Start () {
+        angle=0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float da = 0;
+        if(Input.GetKey(positive))
+            da+=1;
+        if(Input.GetKey(negative))
+            da+=-1;
+
+        if(posJoy!=""){
+        	float axis=Input.GetAxis(posJoy);
+        	axis = axis*axis *Mathf.Sign(axis);
+        	if(Mathf.Abs(axis)<=0.3)
+        		axis=0;
+        	else{
+        		axis = Mathf.Sign(axis)*(Mathf.Abs(axis)-0.3F)/0.7F;
+        	}
+            da+=axis;
+        }
+        if(negJoy!=""){
+        	float axis=Input.GetAxis(negJoy);
+        	axis = axis*axis *Mathf.Sign(axis);
+        	if(Mathf.Abs(axis)<=0.3)
+        		axis=0;
+        	else{
+        		axis = Mathf.Sign(axis)*(Mathf.Abs(axis)-0.3F)/0.7F;
+        	}
+            da-=axis;
+        }
+        if(posButton != "")
+            da+=Input.GetButton(posButton)?1:0;
+        if(negButton != "")
+            da-=Input.GetButton(negButton)?1:0;
+        
+        da = rotationSpeed*Time.deltaTime*da;
+        if(angle+da > maxAngle)
+            da=maxAngle-angle;
+        else if(angle + da < minAngle)
+            da = minAngle-angle;
+
+        angle+=da;
+        transform.Rotate(localRotationAxis, da, Space.Self);
+	}
+}
